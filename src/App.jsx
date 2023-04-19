e5315bcb911f135835201a3a23d36235c87ab469
Nurul Azizah <idlionora@gmail.com>
2023-04-19 21:59:47 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Button from '@mui/material/Button';
+import './App.css'
+
+function App() {
+
+  return (
+		<>
+			<h1 className="text-3xl font-bold underline">Hello World!</h1>
+			<Button className='pb-20' variant="contained">Contained</Button>
+		</>
+  );
+}
+
+export default App
