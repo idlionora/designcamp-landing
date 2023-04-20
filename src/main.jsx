e52ba607049b0b5eb64e3968c5e48cc43c4779d0
Nurul Azizah <idlionora@gmail.com>
2023-04-20 11:32:58 +0700

    config default theme and add react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import { StyledEngineProvider } from "@mui/material";
-import './index.css'
+import { StyledEngineProvider, createTheme, ThemeProvider } from '@mui/material';
+import App from './App';
+import './index.css';
+
+const themeOveride = createTheme({
+	typography: {
+		fontFamily: 'Plus Jakarta Sans',
+		fontWeightLight: 400,
+		fontWeightRegular:500,
+		fontWeightMedium: 600,
+		fontWeightBold: 700
+	},
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<StyledEngineProvider injectFirst>
-			<App />
+			<ThemeProvider theme={themeOveride}>
+				<App />
+			</ThemeProvider>
 		</StyledEngineProvider>
 	</React.StrictMode>
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
-  corePlugins: {
-    preflight: false,
-  },
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	corePlugins: {
+		preflight: false,
+	},
+	theme: {
+		extend: {
+			colors: {
+				'theme-yellow': '#FFA500',
+				'neutral-creme': '#FFF1D9',
+				'neutral-dark': '#1C2024',
+				'theme-grey': '#23262A',
+			},
+		},
+		fontFamily: {
+			text: ['Plus Jakarta Sans', 'sans-serif'],
+			heading: ['Montagu Slab', 'serif'],
+		},
+	},
+	plugins: [],
+};
 
